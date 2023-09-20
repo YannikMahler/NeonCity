beaf6b1852aaf6ba46a05c06dc4ad39084fff001
YannikMahler <yannik.mahler1@outlook.de>
2023-09-20 20:23:54 +0200

    Deletion of the Luminance Parameter in Modblocks

--- src/main/java/de/krypt0n/neoncity/block/ModBlocks.java
+++ src/main/java/de/krypt0n/neoncity/block/ModBlocks.java
@@ -13,62 +13,62 @@ import net.minecraft.util.Identifier;
 public class ModBlocks {
 
     public static final Block BLUE_NEON_LAMP = registerBlocks("blue_neon_lamp",
-            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN).luminance(state -> 20)));
+            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN)));
     public static final Block DARK_BLUE_NEON_LAMP = registerBlocks("dark_blue_neon_lamp",
-            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN).luminance(state -> 20)));
+            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN)));
     public static final Block GREEN_NEON_LAMP = registerBlocks("green_neon_lamp",
-            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN).luminance(state -> 20)));
+            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN)));
     public static final Block MAGENTA_NEON_LAMP = registerBlocks("magenta_neon_lamp",
-            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN).luminance(state -> 20)));
+            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN)));
     public static final Block ORANGE_NEON_LAMP = registerBlocks("orange_neon_lamp",
-            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN).luminance(state -> 20)));
+            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN)));
     public static final Block PINK_NEON_LAMP = registerBlocks("pink_neon_lamp",
-            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN).luminance(state -> 20)));
+            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN)));
     public static final Block RED_NEON_LAMP = registerBlocks("red_neon_lamp",
-            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN).luminance(state -> 20)));
+            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN)));
     public static final Block WHITE_NEON_LAMP = registerBlocks("white_neon_lamp",
-            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN).luminance(state -> 20)));
+            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN)));
     public static final Block YELLOW_NEON_LAMP = registerBlocks("yellow_neon_lamp",
-            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN).luminance(state -> 20)));
+            new Block(FabricBlockSettings.copy(Blocks.SEA_LANTERN)));
 
     public static final Block BLUE_ROD = registerBlocks("blue_rod",
-            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD).luminance(state -> 20)));
+            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD)));
     public static final Block DARK_BLUE_ROD = registerBlocks("dark_blue_rod",
-            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD).luminance(state -> 20)));
+            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD)));
     public static final Block GREEN_ROD = registerBlocks("green_rod",
-            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD).luminance(state -> 20)));
+            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD)));
     public static final Block MAGENTA_ROD = registerBlocks("magenta_rod",
-            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD).luminance(state -> 20)));
+            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD)));
     public static final Block ORANGE_ROD = registerBlocks("orange_rod",
-            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD).luminance(state -> 20)));
+            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD)));
     public static final Block PINK_ROD = registerBlocks("pink_rod",
-            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD).luminance(state -> 20)));
+            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD)));
     public static final Block RED_ROD = registerBlocks("red_rod",
-            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD).luminance(state -> 20)));
+            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD)));
     public static final Block WHITE_ROD = registerBlocks("white_rod",
-            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD).luminance(state -> 20)));
+            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD)));
     public static final Block YELLOW_ROD = registerBlocks("yellow_rod",
-            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD).luminance(state -> 20)));
+            new EndRodBlock(FabricBlockSettings.copy(Blocks.END_ROD)));
 
 
     public static final Block BLUE_BUTTON = registerBlocks("blue_button",
-            new ButtonBlock(FabricBlockSettings.copy(Blocks.POLISHED_BLACKSTONE_BUTTON).luminance(state -> 20),BlockSetType.IRON, 10, true));
+            new ButtonBlock(FabricBlockSettings.copy(Blocks.POLISHED_BLACKSTONE_BUTTON),BlockSetType.IRON, 10, true));
     public static final Block DARK_BLUE_BUTTON = registerBlocks("dark_blue_button",
-            new ButtonBlock(FabricBlockSettings.copy(Blocks.POLISHED_BLACKSTONE_BUTTON).luminance(state -> 20),BlockSetType.IRON, 10, true));
+            new ButtonBlock(FabricBlockSettings.copy(Blocks.POLISHED_BLACKSTONE_BUTTON),BlockSetType.IRON, 10, true));
     public static final Block GREEN_BUTTON = registerBlocks("green_button",
-            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK).luminance(state -> 20),BlockSetType.IRON, 10, true));
+            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK),BlockSetType.IRON, 10, true));
     public static final Block MAGENTA_BUTTON = registerBlocks("magenta_button",
-            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK).luminance(state -> 20),BlockSetType.IRON, 10, true));
+            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK),BlockSetType.IRON, 10, true));
     public static final Block ORANGE_BUTTON = registerBlocks("orange_button",
-            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK).luminance(state -> 20),BlockSetType.IRON, 10, true));
+            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK),BlockSetType.IRON, 10, true));
     public static final Block PINK_BUTTON = registerBlocks("pink_button",
-            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK).luminance(state -> 20),BlockSetType.IRON, 10, true));
+            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK),BlockSetType.IRON, 10, true));
     public static final Block RED_BUTTON = registerBlocks("red_button",
-            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK).luminance(state -> 20),BlockSetType.IRON, 10, true));
+            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK),BlockSetType.IRON, 10, true));
     public static final Block WHITE_BUTTON = registerBlocks("white_button",
-            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK).luminance(state -> 20),BlockSetType.IRON, 10, true));
+            new ButtonBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK),BlockSetType.IRON, 10, true));
     public static final Block YELLOW_BUTTON = registerBlocks("yellow_button",
-            new ButtonBlock(FabricBlockSettings.copy(Blocks.LANTERN).luminance(state -> 20),BlockSetType.IRON, 10, true));
+            new ButtonBlock(FabricBlockSettings.copy(Blocks.LANTERN),BlockSetType.IRON, 10, true));
 
 
 
